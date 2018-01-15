179f379a3fa61c0c38edab66f9e6ebed7121f2f1
budi1200 <alenab22@gmail.com>
2018-01-15 10:54:07 +0100

    jquery

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 document.getElementById("navadna").focus();
 
 window.addEventListener("keydown", function (event) {
-  if (event.defaultPrevented) {
-    return; // Do nothing if the event was already processed
-  }
+    if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
+    }
 
-  switch (event.key) {
-    case "a":
-      document.getElementById("navadna").focus();
-      break;
-    case "d":
-      document.getElementById("posebna").focus();
-      break;
-    default:
-      return; // Quit when this doesn't handle the key event.
-  }
+    switch (event.key) {
+        case "a":
+        case "A":
+            document.getElementById("navadna").focus();
+            break;
+        case "d":
+        case "D":
+            document.getElementById("posebna").focus();
+            break;
+        case "Escape":
+            //window.history.back();
+            window.location.href = 'index.html';
+            break;
 
-  // Cancel the default action to avoid it being handled twice
-  event.preventDefault();
+        default:
+            return; // Quit when this doesn't handle the key event.
+    }
+
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
 }, true);
