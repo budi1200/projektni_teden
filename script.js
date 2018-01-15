a204f9ec73b22436d4869971ce603db914b0efab
budi1200 <alenab22@gmail.com>
2018-01-15 10:44:22 +0100

    prva stran + naslednja stran

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("navadna").focus();
+
+window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+
+  switch (event.key) {
+    case "a":
+      document.getElementById("navadna").focus();
+      break;
+    case "d":
+      document.getElementById("posebna").focus();
+      break;
+    default:
+      return; // Quit when this doesn't handle the key event.
+  }
+
+  // Cancel the default action to avoid it being handled twice
+  event.preventDefault();
+}, true);
