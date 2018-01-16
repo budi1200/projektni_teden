76d4c71b45e35d0d737691a6f75b863dc8547b06
budi1200 <alenab22@gmail.com>
2018-01-16 12:21:19 +0100

    vaja barve

---
+++ posebna_tipkovnica/barve/avto/script.js
@@ -0,0 +1,101 @@
+$("#red").focus();
+var ar = ["#red", "#green", "#blue", "#yellow"];
+var i = 0;
+var barva = 0;
+var popup = false;
+$(document).ready(function () {
+    var rand = Math.floor((Math.random() * 4) + 1);
+    var avto = $("#avto");
+    var width = $(document).width();
+    switch (rand) {
+        case 1:
+            console.log("1 - rdeca");
+            avto.attr("src", "avto_red.png");
+            barva = "red";
+            break;
+        case 2:
+            console.log("2 - zelena");
+            avto.attr("src", "avto_green.png");
+            barva = "green";
+            break;
+        case 3:
+            console.log("3 - modra");
+            avto.attr("src", "avto_blue.png");
+            barva = "blue";
+            break;
+        case 4:
+            console.log("4 - rumena");
+            avto.attr("src", "avto_yellow.png");
+            barva = "yellow";
+            break;
+        default:
+            break;
+    }
+
+    setTimeout(
+        function () {
+            avto.css("transition", "linear 10s");
+            width = width + 201;
+            avto.css("transform", "translatex(" + width + "px)");
+        }, 2000);
+});
+
+$(".buttons a").click(function () {
+    var clicked = $(this).attr("id");
+    popup = true;
+    if (barva == clicked) {
+        console.log("prav");
+        $("#popup_txt").html("Pravilen Odgovor!").css("color", "green");
+        $("#popup_wrapper").css("display", "flex");
+    } else {
+        console.log("narobe");
+        $("#popup_txt").html("Napačen Odgovor!").css("color", "red");
+        $("#popup_wrapper").css("display", "flex");
+    }
+    $("#try_again").focus();
+});
+
+$("#avto").on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
+    function () {
+        console.log("done");
+    });
+
+function change_focus(a) {
+    switch (a) {
+        case '-':
+            i--;
+            break;
+        case '+':
+            i++;
+            break;
+        default:
+            break;
+    }
+    if (i == 4) {
+        i = 0;
+    } else if (i < 0) {
+        i = 3;
+    }
+    $(ar[i]).focus();
+}
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+    switch (event.key) {
+        case "a":
+        case "A":
+            change_focus('-');
+            break;
+        case "d":
+        case "D":
+            change_focus('+');
+            break;
+        case "Escape":
+            window.location.href = "../index.html";
+            break;
+        default:
+            return;
+    }
+    event.preventDefault();
+}, true);
